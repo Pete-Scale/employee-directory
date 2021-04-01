8f45c7bb5be026b358216bd267e7a542e2933370
Pete-Scale <petescale@gmail.com>
2021-04-01 17:53:42 -0500

    made App.js the smart component and filter now works

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import API from './utils/API'
 import Wrapper from './components/Wrapper'
 import Table from './components/Table'
 import Header from './components/Header'
@@ -7,11 +8,36 @@ import Search from './components/Search'
 class App extends Component {
   
   state = {
-    countrySearched: ''
+    countrySearched: '',
+    employees: [],
+    shownEmployees: []
+  }
+  
+  componentDidMount() {
+    API.randomEmployee()
+      .then(response => {
+        // this.updateEmployees(response.data.results)
+        this.setState({ 'employees': response.data.results, 'shownEmployees': response.data.results })
+        // console.log(this.state.employees)
+      })
+      .catch(err => console.log(err))
+  }
+
+  updateEmployees() {
+    const filteredResults = this.state.employees.filter(employee => employee.location.country === this.state.countrySearched)
+    // this.setState({ 'shownEmployees': filteredResults })
+    console.log('results', filteredResults)
+    if(filteredResults.length) {
+      this.setState({'shownEmployees': filteredResults})
+    } else {
+      this.setState({'shownEmployees': this.state.employees})
+    }
   }
 
   findCountry = (country) => {
-    this.setState({'countrySearched': country})
+    this.setState({'countrySearched': country}, () => {
+      this.updateEmployees()
+    })
   }
 
   render() {
@@ -19,7 +45,7 @@ class App extends Component {
           <Wrapper>
             <Header />
             <Search findCountry={this.findCountry} />
-            <Table countrySearched={this.state.countrySearched} />
+            <Table shownEmployees={this.state.shownEmployees} />
           </Wrapper>
     );
   }
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,57 +1,32 @@
-import React, { Component } from 'react'
-import API from '../../utils/API'
+import React from 'react'
 import Employee from '../Employee'
 import TableHeader from '../TableHeader'
 import './style.css'
 
-class Table extends Component {
-  state = {
-    employees: [],
-    shownEmployees: []
-  }
-
-  componentDidMount() {
-    API.randomEmployee()
-      .then(response => {
-        // this.updateEmployees(response.data.results)
-        this.setState({ 'employees': response.data.results, 'shownEmployees': response.data.results })
-        console.log(this.state.employees)
-      })
-      .catch(err => console.log(err))
-  }
-
-  updateEmployees() {
-    const filteredResults = this.state.employees.filter(employee => employee.location.country === this.props.countrySearched)
-    // this.setState({ 'shownEmployees': filteredResults })
-    console.log('results', filteredResults)
-  }
-
-  render() {
-    this.updateEmployees()
-    return (
-        <div className="table-responsive table-styles">
-          <table className="table">
-            <TableHeader />
-            <tbody>
-              {this.state.shownEmployees.map((employee, index) => {
-                return (
-                  <Employee 
-                    key={index}
-                    employeeKey={index+1}
-                    image={employee.picture.thumbnail}
-                    firstName={employee.name.first}
-                    lastName={employee.name.last}
-                    phone={employee.phone}
-                    email={employee.email}
-                    location={employee.location.country}
-                  />
-                )
-              })}
-            </tbody>
-          </table>
-        </div>
-    )
-  }
+function Table(props) {
+  return (
+    <div className="table-responsive table-styles">
+      <table className="table">
+        <TableHeader />
+        <tbody>
+          {props.shownEmployees.map((employee, index) => {
+            return (
+              <Employee
+                key={index}
+                employeeKey={index + 1}
+                image={employee.picture.thumbnail}
+                firstName={employee.name.first}
+                lastName={employee.name.last}
+                phone={employee.phone}
+                email={employee.email}
+                location={employee.location.country}
+              />
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default Table
