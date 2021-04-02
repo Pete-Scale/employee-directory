6206ff66c2d36d6868a71f5968456797efaf9950
Pete-Scale <petescale@gmail.com>
2021-04-02 13:59:42 -0500

    styles and new readme picture

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -7,11 +7,11 @@ function Search(props) {
 
   return (
     <InputGroup className="search-styles mb-3">
-      <InputGroup.Prepend>
+      {/* <InputGroup.Prepend>
         <InputGroup.Text id="basic-addon1">Search</InputGroup.Text>
-      </InputGroup.Prepend>
-      <FormControl
-        placeholder="Country Name"
+      </InputGroup.Prepend> */}
+      <FormControl className="center"
+        placeholder="Filter Employee List By Country"
         aria-label="Country Name"
         aria-describedby="basic-addon1"
         onChange={(e)=> props.findCountry(e.target.value)}
